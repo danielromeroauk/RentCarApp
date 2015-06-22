b827d23074f8ae3beee5903794486f259c45659a
luckys <cfour@openmailbox.org>
2015-06-22 11:02:09 +0100

    Rollbacl modules-2.0.17

---
+++ vendor/pingpong/modules/Providers/BootstrapServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Pingpong\Modules\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BootstrapServiceProvider extends ServiceProvider
+{
+    /**
+     * Booting the package.
+     */
+    public function boot()
+    {
+        $this->app['modules']->register();
+
+        $this->app['modules']->boot();
+    }
+
+    /**
+     * Register the provider.
+     */
+    public function register()
+    {
+        //
+    }
+}
